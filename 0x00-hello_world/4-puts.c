3d0d0ae1a6553a0e4ec10c4842fdf7e8764db7ea
Azeezat Busari <busariazeezat@gmail.com>
2022-06-02 19:20:45 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 
 /**
- *   my_function - This is a description
+ * my_function - This is a description
  */
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
